8741c6cb564d63256ef7261bac727c0698be3054
Jesus Chamorro <jesusfeli54@gmail.com>
2021-11-25 00:39:50 -0500

    Creación del entorno de trabajo

--- app/views.py
+++ app/views.py
@@ -2,10 +2,29 @@ from flask import Flask, render_template, blueprints, request, send_file, redire
 from werkzeug.security import check_password_hash, generate_password_hash
 from markupsafe import escape
 import functools
-import json
+from datetime import datetime, timedelta, date
 
 main = blueprints.Blueprint('main', __name__)
 
-@main.route('/')
-def index():   
+
+def login_required(view):
+    @functools.wraps(view)
+    def wraped_view(**kwargs):
+        if 'usr_email' not in session:
+            return redirect(url_for('main.login'))
+        return view(**kwargs)   
+    return wraped_view
+
+
+@main.route('/',)
+def index():
     return render_template('index.html')
+
+
+@main.route('/login/')
+def login():
+    return render_template('login.html')
+
+@main.route('/sign-in/')
+def register():
+    return render_template('sign_in.html')
